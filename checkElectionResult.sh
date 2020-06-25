5adeeea719f29fe22420ec582527baa5c6b17b8d
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-06-25 02:19:52 +0000

    Update checkElectionResult.sh

--- checkElectionResult.sh
+++ checkElectionResult.sh
@@ -8,7 +8,7 @@ HALF_PUBKEY=$(cat ~/ton-keys/elections/$HOSTNAME-request-dump2 | grep 'public ke
 FULL_PUBKEY=$(printf "0x%s" "$HALF_PUBKEY")
 
 #2 - check if pubkey is participating in current election and the staked amount
-CHECK_PARTICIPATION=$(cd ~/net.ton.dev/tonos-cli/target/release && ./tonos-cli runget -1:3333333333333333333333333333333333333333333333333333333333333333 participates_in $FULL_PUBKEY | grep Result | awk '{print $2}' | tr -d '[' | tr -d '"' | tr -d ']')
+CHECK_PARTICIPATION=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc "runmethodfull -1:3333333333333333333333333333333333333333333333333333333333333333 participates_in ${FULL_PUBKEY}" -rc 'quit' 2>/dev/null | awk 'FNR == 5 {print $3}')
 
 #3 - get active-election-id of new election
 CYCLE_DURATION=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc 'getconfig 15' -rc 'quit' 2>/dev/null | awk 'FNR == 4 {print $4}' | tr -d 'validators_elected_for:')
