49d5e2913c446462446eca5a92d15565dc5facb6
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-06-25 22:46:37 +0000

    Update checkAccount.sh
    #3 - added account balance

--- scripts/checkAccount.sh
+++ scripts/checkAccount.sh
@@ -9,21 +9,28 @@ MY_RAW_ADDRESS=$(cat ~/ton-keys/$HOSTNAME.addr)
 #2 - get my account info
 MY_ACCOUNT=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc "getaccount ${MY_RAW_ADDRESS}" -rc 'quit' 2>/dev/null)
 
-#3 - parse account status
+#3 - parse account balance
+MY_BALANCE=$(echo "$MY_ACCOUNT" | tail -n 1 | tr -d '=''ng' | awk '{print $4}')
+CONVERTED_MY_BALANCE=$(echo $MY_BALANCE/1000000000 | bc -l)
+ROUNDED_MY_BALANCE=$(printf "%.9f" $CONVERTED_MY_BALANCE)
+
+#4 - parse account status
 ACCOUNT_STATUS=$(echo "$MY_ACCOUNT" | grep account | awk 'FNR == 3 {print $1}' | perl -ne '/state:\(account_(\S+)/ && print $1')
 
-#4 - parse last transaction time
+#5 - parse last transaction time
 LAST_TRANS_LT=$(echo "$MY_ACCOUNT" | grep account | awk 'FNR == 2 {print $2}' | tr -d 'last_trans_lt:')
 
-#5 parse last transaction
+#6 parse last transaction
 LAST_TIME=$(echo "$MY_ACCOUNT" | grep last_paid | awk '{print $4}' | tr -d 'last_paid:')
 CONVERTED_LAST_TIME=$(date -d @"$LAST_TIME")
 
-#6 - print account info
+#7 - print account info
 printf "Address: "
 echo $MY_RAW_ADDRESS
 printf "Status: "
 echo $ACCOUNT_STATUS
+printf "Balance: "
+echo $ROUNDED_MY_BALANCE
 printf "Last Transaction Logical Time: "
 echo $LAST_TRANS_LT
 printf "Last Transaction Time: "
