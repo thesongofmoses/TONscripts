91cf801317106759d81670df9c95bd702fd06a24
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-06-25 09:45:14 +0000

    Update checkAccount.sh

--- checkAccount.sh
+++ checkAccount.sh
@@ -10,21 +10,22 @@ MY_RAW_ADDRESS=$(cat ~/ton-keys/$HOSTNAME.addr)
 MY_ACCOUNT=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc "getaccount ${MY_RAW_ADDRESS}" -rc 'quit' 2>/dev/null)
 
 #3 - parse account status
-ACCOUNT_STATUS=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc "getaccount ${MY_RAW_ADDRESS}" -rc 'quit' 2>/dev/null | grep account | awk 'FNR == 3 {print $1}' | perl -ne '/state:\(account_(\S+)/ && print $1')
+ACCOUNT_STATUS=$(echo "$MY_ACCOUNT" | grep account | awk 'FNR == 3 {print $1}' | perl -ne '/state:\(account_(\S+)/ && print $1')
 
 #4 - parse last transaction time
-LAST_TRANS_LT=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc "getaccount ${MY_RAW_ADDRESS}" -rc 'quit' 2>/dev/null | grep account | awk 'FNR == 2 {print $2}' | tr -d 'last_trans_lt:')
+LAST_TRANS_LT=$(echo "$MY_ACCOUNT" | grep account | awk 'FNR == 2 {print $2}' | tr -d 'last_trans_lt:')
 
 #5 parse last transaction
-LAST_PAID=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc "getaccount ${MY_RAW_ADDRESS}" -rc 'quit' 2>/dev/null | grep last_paid | awk '{print $4}' | tr -d 'last_paid:')
-CONVERTED_LAST_PAID=$(echo $LAST_PAID/1000000000 | bc -l)
+LAST_PAID=$(echo "$MY_ACCOUNT" | grep last_paid | awk '{print $4}' | tr -d 'last_paid:')
+CONVERTED_LAST_PAID=$(echo "$LAST_PAID"/1000000000 | bc -l)
+ROUNDED_LAST_PAID=$(printf "%.9f" $CONVERTED_LAST_PAID)
 
 #6 - print account info
-printf "Account Address: "
+printf "Address: "
 echo $MY_RAW_ADDRESS
-printf "Account Status: "
+printf "Status: "
 echo $ACCOUNT_STATUS
 printf "Last Transaction Logical Time: "
 echo $LAST_TRANS_LT
 printf "Last Transaction Amount: "
-echo $CONVERTED_LAST_PAID
+echo $ROUNDED_LAST_PAID
