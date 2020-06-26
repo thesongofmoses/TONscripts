f0366debfea65ca5bb9ce289d3f572d449f63e1c
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-06-26 11:42:13 +0000

    Update checkElection.sh
    #5, #6, #8, #9 - replaced election start time with end time

--- scripts/checkElection.sh
+++ scripts/checkElection.sh
@@ -10,38 +10,47 @@ FULL_PUBKEY=$(printf "0x%s" "$HALF_PUBKEY")
 #2 - check if pubkey is participating in current election and the staked amount
 CHECK_PARTICIPATION=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc "runmethodfull -1:3333333333333333333333333333333333333333333333333333333333333333 participates_in ${FULL_PUBKEY}" -rc 'quit' 2>/dev/null | awk 'FNR == 5 {print $3}')
 
-#3 - get active-election-id of new election
+#3 - get active-election-id of new election from getconfig34
+CURRENT_ELECTION_ID=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc 'getconfig 34' -rc 'quit' 2>/dev/null | grep utime | awk '{print $2}' | tr -d 'utime_since:')
+
+#4 - get cycle duration from getconfig15
 CYCLE_DURATION=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc 'getconfig 15' -rc 'quit' 2>/dev/null | awk 'FNR == 4 {print $4}' | tr -d 'validators_elected_for:')
-PREVIOUS_ELECTION_ID=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc 'getconfig 34' -rc 'quit' 2>/dev/null | grep utime | awk '{print $2}' | tr -d 'utime_since:')
 
-#4 = get the current election human time
-CURRENT_ELECTION_TIME=$(date -d @$PREVIOUS_ELECTION_ID)
+#5 - get end time of upcoming election in unixtime
+UNTIL_TIMESTAMP=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc 'getconfig 34' -rc 'quit' 2>/dev/null | grep time | awk '{print $3}' | tr -d 'utime_until:')
+ELECTION_END_BEFORE=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc 'getconfig 15' -rc 'quit' 2>/dev/null | awk 'FNR == 4 {print $6}' | tr -d 'elections_end_before:')
+UPCOMING_ELECTION_END_UNIXTIME=$(expr $UNTIL_TIMESTAMP - $ELECTION_END_BEFORE)
 
-#5 - convert the participating stake amount to regular unit
+#6 = get the end time of upcoming election in human time
+UPCOMING_ELECTION_END_HUMANTIME=$(date -d @"$UPCOMING_ELECTION_END_UNIXTIME")
+
+#7 - convert the participating stake amount to regular unit
 UNCONVERTED_TOKENS=$(printf "%d\n" "$CHECK_PARTICIPATION")
 CONVERTED_TOKENS=$(echo "$UNCONVERTED_TOKENS"/1000000000 | bc -l)
 ROUNDED_TOKENS=$(printf "%.9f" $CONVERTED_TOKENS)
 
-#6 - check if Elector confirms participation in current election and the amount
+#8 - check if Elector confirms participation in current election and the amount
 if [ "$ROUNDED_TOKENS" != "0.000000000" ]; then
         echo "---------CURRENT ELECTION---------"
         printf "ID: "
         echo "$PREVIOUS_ELECTION_ID"
         printf "Staked Tokens: "
         echo "$ROUNDED_TOKENS"
-        echo "---$CURRENT_ELECTION_TIME---"
+        echo '-------THIS ELECTION CLOSES-------'
+        echo "-$UPCOMING_ELECTION_END_HUMANTIME-"
         echo "-------------CONFIRMED------------"
 
-#7 - if amount equals zero, print warning msg
+#9 - if amount equals zero, print warning msg
 elif [ "$ROUNDED_TOKENS" == "0.000000000" ]; then
         echo "WARNING: Election submission failed"
         printf "Current Election ID: "
-        echo "$PREVIOUS_ELECTION_ID"
+        echo "$CURRENT_ELECTION_ID"
         printf "Staked Tokens: "
         echo "$ROUNDED_TOKENS"
-        echo "$CURRENT_ELECTION_TIME"
+        printf 'This election closes: '
+        echo "$UPCOMING_ELECTION_END_HUMANTIME"
 
-#8 - else, print unknown error msg
+#10 - else, print unknown error msg
 else
         echo "WARNING: Unknown Error"
 fi
