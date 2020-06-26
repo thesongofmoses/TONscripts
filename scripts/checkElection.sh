b13e9ad9f6026c9a5b543b64c6419e7cf406cfed
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-06-26 11:24:09 +0000

    Update checkElection.sh
    #5 - added decimals up to 9

--- scripts/checkElection.sh
+++ scripts/checkElection.sh
@@ -8,36 +8,40 @@ HALF_PUBKEY=$(cat ~/ton-keys/elections/$HOSTNAME-request-dump2 | grep 'public ke
 FULL_PUBKEY=$(printf "0x%s" "$HALF_PUBKEY")
 
 #2 - check if pubkey is participating in current election and the staked amount
-CHECK_PARTICIPATION=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc "runmethodfull -1:3333333333333333333333333333333333333333333333333333333333333333 participates_in ${FULL_PUBKEY}" -rc 'quit' 2>/dev/null | awk 'FNR == 5 {print $3}')
+CHECK_PARTICIPATION=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc "runmethodfull -1:33333333333333333333333333333333333333333333333$
 
 #3 - get active-election-id of new election
-CYCLE_DURATION=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc 'getconfig 15' -rc 'quit' 2>/dev/null | awk 'FNR == 4 {print $4}' | tr -d 'validators_elected_for:')
-PREVIOUS_ELECTION_ID=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc 'getconfig 34' -rc 'quit' 2>/dev/null | grep utime | awk '{print $2}' | tr -d 'utime_since:')
-CURRENT_ELECTION_ID=$(expr $PREVIOUS_ELECTION_ID + $CYCLE_DURATION)
+CYCLE_DURATION=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc 'getconfig 15' -rc 'quit' 2>/dev/null | awk 'FNR == 4 {print $4}' | tr$
+PREVIOUS_ELECTION_ID=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc 'getconfig 34' -rc 'quit' 2>/dev/null | grep utime | awk '{print$
 
-#4 - convert the participating stake amount to regular unit
+#4 = get the current election human time
+CURRENT_ELECTION_TIME=$(date -d @$PREVIOUS_ELECTION_ID)
+
+#5 - convert the participating stake amount to regular unit
 UNCONVERTED_TOKENS=$(printf "%d\n" "$CHECK_PARTICIPATION")
-CONVERTED_TOKENS=$(expr "$UNCONVERTED_TOKENS" / 1000000000)
+CONVERTED_TOKENS=$(echo "$UNCONVERTED_TOKENS"/1000000000 | bc -l)
+ROUNDED_TOKENS=$(printf "%.9f" $CONVERTED_TOKENS)
 
-#5 - check if Elector confirms participation in current election and the amount
-if [ "$CONVERTED_TOKENS" != "0" ]; then
-        echo "-----------CONFIRMED-----------"
-        printf "Current Election ID: "
-        echo "$CURRENT_ELECTION_ID"
+#6 - check if Elector confirms participation in current election and the amount
+if [ "$ROUNDED_TOKENS" != "0.000000000" ]; then
+        echo "---------CURRENT ELECTION---------"
+        printf "ID: "
+        echo "$PREVIOUS_ELECTION_ID"
         printf "Staked Tokens: "
-        echo "$CONVERTED_TOKENS"
-        echo "-----------CONFIRMED-----------"
-        
-#6 - if amount equals zero, print warning msg
-elif [ "$unconvertedTokens" == "0" ]; then
+        echo "$ROUNDED_TOKENS"
+        echo "---$CURRENT_ELECTION_TIME---"
+        echo "-------------CONFIRMED------------"
+
+#7 - if amount equals zero, print warning msg
+elif [ "$ROUNDED_TOKENS" == "0.000000000" ]; then
         echo "WARNING: Election submission failed"
-        echo ""
         printf "Current Election ID: "
-        echo "$CURRENT_ELECTION_ID"
+        echo "$PREVIOUS_ELECTION_ID"
         printf "Staked Tokens: "
-        echo "$CONVERTED_TOKENS"
+        echo "$ROUNDED_TOKENS"
+        echo "$CURRENT_ELECTION_TIME"
 
-#7 - else, print unknown error msg
+#8 - else, print unknown error msg
 else
         echo "WARNING: Unknown Error"
 fi
