d947af9ae9285a6cbc40ee40d0248dfda51ab840
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-01 11:05:11 +0000

    Update validatorRestart.sh

--- scripts/validatorRestart.sh
+++ scripts/validatorRestart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CHECK_ENGINE=$(pgrep -f '/home/ton/net.ton.dev/ton/build/validator-engine/validator-engine -C /var/ton-work/etc/ton-global.config.json --db /var/ton-work/db')
+CHECK_ENGINE=$(pgrep -f '~/net.ton.dev/ton/build/validator-engine/validator-engine -C /var/ton-work/etc/ton-global.config.json --db /var/ton-work/db')
 PRINT_UNIXTIME=$(date +%s)
 
 function RESTART () {
