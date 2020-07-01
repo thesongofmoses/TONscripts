26146f417be48509684831b8e96cf9e90563265a
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-01 23:20:05 +0000

    Update validatorManual.sh

--- scripts/validatorManual.sh
+++ scripts/validatorManual.sh
@@ -8,6 +8,8 @@ BOC=$(cd ~/ton-keys/elections && echo $(base64 --wrap=0 "validator-query.boc"))
 MSIG_KEYS_JSON="~/ton-keys/msig.keys.json"
 ABI_JSON="~/net.ton.dev/configs/SafeMultisigWallet.abi.json"
 ECHO_0_ELECTION=$(cd ~/ton-keys/elections && echo 0 > active-election-id && echo 0 > election-id)
+ACTIVE_ELECTION_ID=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc 'getconfig 34' -rc 'quit' 2>/dev/null | grep time | awk '{print $3}' | tr -d 'utime_until:')
+ECHO_ID_ELECTION=$(cd ~/ton-keys/elections && echo $ACTIVE_ELECTION_ID > active-election-id && echo $ACTIVE_ELECTION_ID > election-id)
 
 #balance
 #2 - get my account info
@@ -23,4 +25,4 @@ STAKE="$(($RANDOM% $BALANCE_BY+$BALANCE))"
 #command
 submitTransaction=$(cd ~/net.ton.dev/tonos-cli/target/release && ./tonos-cli call "$MY_RAW_ADDRESS" submitTransaction "{"\"dest"\":"\"-1:3333333333333333333333333333333333333333333333333333333333333333"\","\"value"\":"$STAKE","\"bounce"\":false,"\"allBalance"\":false,"\"payload"\":"\"$BOC"\"}" --abi ~/net.ton.dev/configs/SafeMultisigWallet.abi.json --sign ~/ton-keys/msig.keys.json)
 
-$submitTransaction >> ~/node.operator/logs/validator.log
+$ECHO_0_ELECTION && $submitTransaction >> ~/node.operator/logs/validator.log && $ECHO_ID_ELECTION
