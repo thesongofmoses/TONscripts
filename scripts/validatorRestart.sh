8c8deff11f6ea3ee91060c11cf3afaccfd4024d0
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-01 10:47:22 +0000

    Update validatorRestart.sh

--- scripts/validatorRestart.sh
+++ scripts/validatorRestart.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 CHECK_ENGINE=$(pgrep validator)
+PRINT_UNIXTIME=$(date +%s)
 
+function RESTART () {
+        printf '%u\n' "$PRINT_UNIXTIME" && \
+        cd ~/net.ton.dev/scripts/ && \
+        ./run.sh
+}
+
+#if there is no validator-engine process, then execute run.sh
 if [ -z "$CHECK_ENGINE" ]; then
-        cd ~/net.ton.dev/scripts/ && ./run.sh
+        RESTART >> ~/ton-logs/restart.log
 fi
