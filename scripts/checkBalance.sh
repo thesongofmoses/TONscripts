ae261a5fa337b471e7b896dfd9e8aeb509fff6a8
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-02 12:32:31 +0000

    exported all variables to scripts.config

--- scripts/checkBalance.sh
+++ scripts/checkBalance.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
-#VARIABLES
-HOSTNAME=$(cat /etc/hostname)
+. ~/node.operator/configs/scripts.config
 
-#1 - get my raw address
-MY_RAW_ADDRESS=$(cat ~/ton-keys/$HOSTNAME.addr)
-
-#2 - get my account info
-MY_ACCOUNT=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc "getaccount ${MY_RAW_ADDRESS}" -rc 'quit' 2>/dev/null)
-
-#3 - grep balance
-MY_BALANCE=$(echo "$MY_ACCOUNT" | tail -n 1 | tr -d '=''ng' | awk '{print $4}')
-
-#4 - convert tokens to regular unit
-CONVERTED_TOKENS=$(echo "$MY_BALANCE"/1000000000 | bc -l)
-
-#5 display balance up to 9 decimals
-function balance () {
-        printf "Account Balance: "
-        printf "%.9f" $CONVERTED_TOKENS
-        echo ""
-}
-
-balance
+printf "Account Balance: "
+printf "$MY_BALANCE\n"
