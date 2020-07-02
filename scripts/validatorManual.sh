1c96675e48921a253085dd4f250603efd28e4a5c
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-02 15:28:41 +0000

    put variables at top for easy manipulation

--- scripts/validatorManual.sh
+++ scripts/validatorManual.sh
@@ -2,11 +2,14 @@
 
 HOSTNAME=$(cat /etc/hostname)
 
+#unit is nanogram
+BALANCE_BY='1000000000000'
+FEE_RESERVE='10000000000'
+
 #variables
 MY_RAW_ADDRESS=$(cat ~/ton-keys/$HOSTNAME.addr)
 BOC=$(cd ~/ton-keys/elections && echo $(base64 --wrap=0 "validator-query.boc"))
-MSIG_KEYS_JSON="~/ton-keys/msig.keys.json"
-ABI_JSON="~/net.ton.dev/configs/SafeMultisigWallet.abi.json"
+
 ECHO_0_ELECTION=$(cd ~/ton-keys/elections && echo 0 > active-election-id && echo 0 > election-id)
 ACTIVE_ELECTION_ID=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc 'getconfig 34' -rc 'quit' 2>/dev/null | grep time | awk '{print $3}' | tr -d 'utime_until:')
 ECHO_ID_ELECTION=$(cd ~/ton-keys/elections && echo $ACTIVE_ELECTION_ID > active-election-id && echo $ACTIVE_ELECTION_ID > election-id)
@@ -17,9 +20,8 @@ MY_ACCOUNT=$(cd ~/net.ton.dev/ton/build/lite-client && ./lite-client -p ~/ton-ke
 #3 - grep balance
 MY_BALANCE=$(echo "$MY_ACCOUNT" | tail -n 1 | tr -d '=''ng' | awk '{print $4}')
 
-
-BALANCE_BY='1000000000000'
-BALANCE=$(echo $MY_BALANCE-$BALANCE_BY-10000000000 | bc -l)
+#4 - calculate stakes
+BALANCE=$(echo $MY_BALANCE-$BALANCE_BY-$FEE_RESERVE | bc -l)
 STAKE="$(($RANDOM% $BALANCE_BY+$BALANCE))"
 
 #command
