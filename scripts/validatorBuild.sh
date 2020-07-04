70dc9c95d1bda4200dde197a8f990a854f31fa97
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-04 12:03:05 +0000

    echo node version at the end, backup keys dir

--- scripts/validatorBuild.sh
+++ scripts/validatorBuild.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+HOSTNAME=$(cat /etc/hostname)
+
+#backup keys
+mkdir ~/keys.backup && \
+mv ~/ton-keys ~/keys.backup/ton-keys
+
 #delete existing dirs
 sudo rm -rf /var/ton-work \
 rm -rf ~/ton-keys \
@@ -12,6 +18,12 @@ cd && git clone https://github.com/tonlabs/net.ton.dev.git && \
 cd net.ton.dev/scripts && \
 . ./env.sh && \
 ./build.sh && \
+
+#restore keys
+mv ~/keys.backup/ton-keys ~/
+rm -rf ~/keys.backup
+
+#run
 export NETWORK_TYPE="${1}" && \
 ./setup.sh && \
 ./run.sh && \
@@ -19,3 +31,9 @@ export NETWORK_TYPE="${1}" && \
 #set configs for tonos-cli
 cd ~/net.ton.dev/tonos-cli/target/release/ && ./tonos-cli config --url https://"${1}".ton.dev --wc -1 && \
 cp ~/net.ton.dev/tonos-cli/target/release/tonlabs-cli.conf.json ~/net.ton.dev/scripts/tonlabs-cli.conf.json \
+
+#echo node versions
+echo '/ton version:' && \
+cd ~/net.ton.dev/ton && git rev-parse HEAD && \
+echo '/net.ton.dev version:' && \
+cd ~/net.ton.dev && git rev-parse HEAD
