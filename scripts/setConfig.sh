6324a57ba831504b506052e9516d2f85e832b1a3
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-04 09:21:21 +0000

    Delete setConfig.sh

--- scripts/setConfig.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ~/net.ton.dev/tonos-cli/target/release/ && ./tonos-cli config --url https://"${1}".ton.dev --wc -1 && \
-cp ~/net.ton.dev/tonos-cli/target/release/tonlabs-cli.conf.json ~/net.ton.dev/scripts/tonlabs-cli.conf.json \
