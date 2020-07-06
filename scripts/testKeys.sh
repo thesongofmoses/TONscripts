99c054bf8b33ea1c5b3332e6c7552ebf4a5c84f7
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-06 17:52:41 +0000

    Update testKeys.sh

--- scripts/testKeys.sh
+++ scripts/testKeys.sh
@@ -2,8 +2,6 @@
 
 . ~/node.operator/configs/scripts.config
 
-TEST_VALIDATION_PREVIOUS_ADNL_KEY=$(echo "${GETCONFIG32}" | grep $PREVIOUS_ADNL_KEY | awk '{print $4}' | tr -d ')' | tr -d 'adnl_addr:x')
-
 if [ "$NEW_ADNL_KEY" == "$DIR_ELECTION_ADNL_KEY" ];
 then
         echo "new adnl key is dir election adnl key"
@@ -11,7 +9,7 @@ else
         echo "new adnl key is NOT dir election adnl key"
 fi
 
-if [ "$TEST_VALIDATION_PREVIOUS_ADNL_KEY" == "$PREVIOUS_ADNL_KEY" ];
+if [ "$CHECK_VALIDATION_STATUS_PREVIOUS_ADNL_KEY" == "$PREVIOUS_ADNL_KEY" ];
 then
         echo "past adnl key is n-3"
 else
