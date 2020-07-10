fe08000e8d40d4908dab9ae3e969a07e38731428
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-10 04:39:18 +0000

    Update validatorManual.sh

--- scripts/validatorManual.sh
+++ scripts/validatorManual.sh
@@ -3,8 +3,8 @@
 HOSTNAME=$(cat /etc/hostname)
 
 #unit is nanogram
-BALANCE_BY='10000000000'
-FEE_RESERVE='5000000000'
+BALANCE_BY='5000000000'
+FEE_RESERVE='2000000000'
 
 #variables
 MY_RAW_ADDRESS=$(cat ~/ton-keys/$HOSTNAME.addr)
