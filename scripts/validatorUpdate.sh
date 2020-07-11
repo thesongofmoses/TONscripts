9e0058022f406eb7a0c3ee9b6391368398f40939
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-11 18:51:45 +0000

    example: ./validatorUpdate game3

--- scripts/validatorUpdate.sh
+++ scripts/validatorUpdate.sh
@@ -9,3 +9,13 @@ cd ~/net.ton.dev/scripts && ./build.sh
 cd ~/net.ton.dev/scripts && ./run.sh
 cd ~/net.ton.dev/ton
 git rev-parse HEAD
+
+#set configs for tonos-cli
+cd ~/net.ton.dev/tonos-cli/target/release/ && ./tonos-cli config --url https://"${1}".ton.dev --wc -1 && \
+cp ~/net.ton.dev/tonos-cli/target/release/tonlabs-cli.conf.json ~/net.ton.dev/scripts/tonlabs-cli.conf.json \
+
+#echo node versions
+echo '/ton version:' && \
+cd ~/net.ton.dev/ton && git rev-parse HEAD && \
+echo '/net.ton.dev version:' && \
+cd ~/net.ton.dev && git rev-parse HEAD
