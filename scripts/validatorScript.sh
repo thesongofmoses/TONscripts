f5fe48104281991da079087afea2ab8c50ff2d34
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-14 05:36:25 +0000

    added 300s buffer to submitting election before rewards become available for recovery

--- scripts/validatorScript.sh
+++ scripts/validatorScript.sh
@@ -3,14 +3,14 @@
 . ~/node.operator/configs/scripts.config
 
 #variables
-BALANCE_BY='2'
+BALANCE_BY='10'
 SLEEP_BY='60'
 FEES_RESERVE='1'
 
 BALANCE=$(echo $MY_BALANCE-$BALANCE_BY-$FEES_RESERVE | bc -l)
 ROUNDED_BALANCE=$(printf "%.0f" $BALANCE)
 
-SLEEP="$(($RANDOM% $SLEEP_BY))"
+SLEEP="$(($RANDOM% $SLEEP_BY+300))"
 STAKE="$(($RANDOM% $BALANCE_BY+$ROUNDED_BALANCE))"
 
 sleep $SLEEP && cd ~/net.ton.dev/scripts && ./validator_msig.sh $STAKE >> ~/node.operator/logs/validator.log
