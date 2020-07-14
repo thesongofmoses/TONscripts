687926ab7ec76c197666229a81366eabe4fb5b79
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-14 21:07:06 +0000

    Update validatorScript.sh

--- scripts/validatorScript.sh
+++ scripts/validatorScript.sh
@@ -7,10 +7,9 @@ BALANCE_BY='10'
 SLEEP_BY='200'
 FEES_RESERVE='1'
 
-BALANCE=$(echo $MY_BALANCE-$BALANCE_BY-$FEES_RESERVE | bc -l)
-ROUNDED_BALANCE=$(printf "%.0f" $BALANCE)
+BALANCE=$(echo "scale=0; $MY_BALANCE-$BALANCE_BY-$FEES_RESERVE" | bc -l)
 
 SLEEP="$(($RANDOM% $SLEEP_BY+300))"
-STAKE="$(($RANDOM% $BALANCE_BY+$ROUNDED_BALANCE))"
+STAKE="$(($RANDOM% $BALANCE_BY+$BALANCE))"
 
 sleep $SLEEP && cd ~/net.ton.dev/scripts && ./validator_msig.sh $STAKE >> ~/node.operator/logs/validator.log
