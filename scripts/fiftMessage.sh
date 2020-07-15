69091cda5b608241b0021e87d8b18c5d5066436c
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-15 08:09:39 +0000

    ./* <fullpathtoboc>

--- scripts/fiftMessage.sh
+++ scripts/fiftMessage.sh
@@ -2,4 +2,9 @@
 
 USERNAME=$(whoami)
 
-cd ~/net.ton.dev/ton/build/lite-client && ./lite-client  -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc "sendfile /home/$USERNAME/net.ton.dev/ton/build/${1}"
+cd ~/net.ton.dev/ton/build/lite-client && \
+./lite-client \
+-p ~/ton-keys/liteserver.pub \
+-a 127.0.0.1:3031 \
+-rc \
+"sendfile /home/$USERNAME/net.ton.dev/ton/build/${1}"
