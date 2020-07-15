3d9da0641fcd70f6085cbfafa7867ec694354a9e
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-15 18:29:15 +0000

    ./fiftTransaction.sh <wallet-id> <dest> <seq.no> <amount in grams> <savefile>

--- scripts/fiftTransaction.sh
+++ scripts/fiftTransaction.sh
@@ -4,4 +4,5 @@ USERNAME=$(whoami)
 cd ~/net.ton.dev/ton/build && \
 crypto/fift \
 -I/home/$USERNAME/net.ton.dev/ton/crypto/fift/lib \
--s /home/$USERNAME/net.ton.dev/ton/crypto/smartcont/wallet.fif ${1} ${2} ${3} ${4} ${5}
+-s /home/$USERNAME/net.ton.dev/ton/crypto/smartcont/wallet.fif \
+${1} ${2} ${3} ${4} ${5}
