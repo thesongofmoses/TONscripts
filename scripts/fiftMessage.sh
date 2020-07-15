dd3e88cc259566bb3597b01e90e3a073c8ceafbd
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-15 07:30:32 +0000

    Update fiftMessage.sh

--- scripts/fiftMessage.sh
+++ scripts/fiftMessage.sh
@@ -2,4 +2,4 @@
 
 USERNAME=$(whoami)
 
-./lite-client  -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc "sendfile /home/$USERNAME/net.ton.dev/ton/build/${1}"
+cd ~/net.ton.dev/ton/build && ./lite-client  -p ~/ton-keys/liteserver.pub -a 127.0.0.1:3031 -rc "sendfile /home/$USERNAME/net.ton.dev/ton/build/${1}"
