065f5ffb616a6b3fefb3a004abc33a7c8e127c7d
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-15 07:52:34 +0000

    ./fiftTransaction.sh <dest> <seq.no> <amount in grams> <save file name>

---
+++ scripts/fiftTransaction.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+USERNAME=$(whoami)
+
+cd ~/net.ton.dev/ton/build && \
+crypto/fift \
+-I/home/$USERNAME/net.ton.dev/ton/crypto/fift/lib \
+-s /home/$USERNAME/net.ton.dev/ton/crypto/smartcont/wallet.fif ${1} ${2} ${3} ${4} 
