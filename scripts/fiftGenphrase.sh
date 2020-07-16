e7e28c6a5edd7557068e34e0e7c13aa3bc9d053b
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-16 10:58:58 +0000

    save output to.dump
    ./f... <workchain id> <file name>

--- scripts/fiftGenphrase.sh
+++ scripts/fiftGenphrase.sh
@@ -2,9 +2,9 @@
 
 USERNAME=$(whoami)
 
-cd ~/net.ton.dev/ton/build && \
-crypto/fift \
--I/home/$USERNAME/net.ton.dev/ton/crypto/fift/lib \
--s /home/$USERNAME/net.ton.dev/ton/crypto/smartcont/new-wallet.fif \
-"${1}" \
-"${2}"
+GENPHRASE=$(cd ~/net.ton.dev/ton/build/ && \
+        crypto/fift \
+        -I/home/$USERNAME/net.ton.dev/ton/crypto/fift/lib \
+        -s /home/$USERNAME/net.ton.dev/ton/crypto/smartcont/new-wallet.fif "${1}" "${2}")
+
+echo $GENPHRASE | tee ~/net.ton.dev/ton/build/"${2}".dump
