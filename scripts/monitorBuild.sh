b84ceae7dc058cbb6d0394f774ffb03e77c850ed
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-20 10:41:42 +0000

    removed repeated codes

--- scripts/monitorBuild.sh
+++ scripts/monitorBuild.sh
@@ -12,18 +12,13 @@ git clone https://github.com/kevintmax/monitor/ && \
 chmod -R +x ~/node.operator/monitor
 
 #install tg bot
-pip uninstall python-telegram-bot; \
-pip uninstall telegram; \
-pip uninstall telegram-bot; \
+pip uninstall python-telegram-bot telegram telegram-bot
 pip install telegram-bot
 
 #install python, gspread,psutil
-sudo apt install -y python && \
-sudo apt update && \
-sudo apt install -y python-pip && \
-sudo pip install gspread && \
-sudo pip install --upgrade oauth2client && \
-sudo pip install psutil
+sudo apt update
+sudo apt install -y python python-pip && \
+sudo pip install gspread --upgrade oauth2client psutil
 
 #install api key
 cd ~/node.operator/configs && \
@@ -32,6 +27,15 @@ mv "${API_KEY}".json py.json
 
 #define cell for check.py and node # for alarm.py
 cd ~/node.operator/configs && \
-echo "CELL='${CELL}'" > cell.config && ~/node.operator/monitor/check.sh
-cd ~/node.operator/configs && \
-echo "NODE='${NODE}'" > node.config && ~/node.operator/monitor/alarm.sh
+echo "CELL='${CELL}'" > cell.config
+echo "NODE='${NODE}'" > node.config
+
+#cat config to .py
+cat node.config > ~/node.operator/monitor/alarm.py && cat cell.config > ~/node.operator/monitor/check.py
+
+#execute alarm/check.sh
+cd ~/node.operator/monitor
+rm alarm && rm check
+wget https://raw.githubusercontent.com/kevintmax/monitor/master/alarm && wget https://raw.githubusercontent.com/kevintmax/monitor/master/check
+cat ~/node.operator/monitor/alarm >> ~/alarm.py && cat ~/node.operator/monitor/check >> ~/node.operator/monitor/check.py
+rm alarm && rm check
