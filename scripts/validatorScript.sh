f834799920a0ebca1c688a0a98264ac602c13115
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-20 07:21:01 +0000

    Update validatorScript.sh

--- scripts/validatorScript.sh
+++ scripts/validatorScript.sh
@@ -3,16 +3,14 @@
 . ~/node.operator/configs/scripts.config
 
 #variables
-BALANCE_BY='5'
-FEES_RESERVE='2'
+FEES_RESERVE='1'
 SLEEP_BY='180'
 SLEEP_DELAY='90'
 
-BALANCE=$(echo $MY_BALANCE-$BALANCE_BY-$FEES_RESERVE | bc -l)
-ROUNDED_BALANCE=$(printf "%.0f" $BALANCE)
+BALANCE=$(echo $MY_BALANCE-$FEES_RESERVE | bc -l)
+STAKE=$(printf "%.0f" $BALANCE)
 
 SLEEP="$(($RANDOM% $SLEEP_BY+$SLEEP_DELAY))"
-STAKE="$(($RANDOM% $BALANCE_BY+$ROUNDED_BALANCE))"
 
 if [ $CHECK_ELECTION_STATUS != 0 ];
 then
