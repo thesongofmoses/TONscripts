908eadeb14695dc56e7729038ca4dbe75e99dfb1
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-20 04:26:53 +0000

    Create monitorBuild.sh

---
+++ scripts/monitorBuild.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+NODE="${1}"
+API_KEY=$(echo "$NODE" | cut -c 1)
+CELL_UNPARSED=$(expr "$NODE" + 2)
+CELL=$(printf "B%s" "$CELL_UNPARSED")
+
+#install .pys
+rm -rf ~/node.operator/monitor && \
+cd ~/node.operator && \
+git clone https://github.com/kevintmax/monitor/ && \
+chmod -R +x ~/node.operator/monitor
+
+#install tg bot
+pip uninstall -y python-telegram-bot; \
+pip uninstall -y telegram; \
+pip uninstall -y telegram-bot; \
+pip install -y telegram-bot
+
+#INSTALL CHECK.PY
+#install python, gspread,psutil
+sudo apt install -y python && \
+sudo apt update && \
+sudo apt install -y python-pip && \
+sudo pip install gspread && \
+sudo pip install --upgrade oauth2client && \
+sudo pip install psutil
+
+#install api key
+cd ~/node.operator/configs && \
+wget https://raw.githubusercontent.com/kevintmax/pykey/master/"${API_KEY}".json
+
+#define cell for check.py
+cd ~/node.operator/monitor && \
+echo "CELL=${CELL}" > checknode && ./check.sh
+echo "NODE=${NODE}" > alarmnode && ./alarm.sh
