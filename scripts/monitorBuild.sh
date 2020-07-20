ffd18f9cd36e8a2d46501d6765e637cb913acdd4
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-20 11:01:52 +0000

    solved mystery

--- scripts/monitorBuild.sh
+++ scripts/monitorBuild.sh
@@ -37,5 +37,5 @@ cat node.config > ~/node.operator/monitor/alarm.py && cat cell.config > ~/node.o
 cd ~/node.operator/monitor
 rm alarm && rm check
 wget https://raw.githubusercontent.com/kevintmax/monitor/master/alarm && wget https://raw.githubusercontent.com/kevintmax/monitor/master/check
-cat ~/node.operator/monitor/alarm >> ~/alarm.py && cat ~/node.operator/monitor/check >> ~/node.operator/monitor/check.py
+cat ~/node.operator/monitor/alarm >> ~/node.operator/monitor/alarm.py && cat ~/node.operator/monitor/check >> ~/node.operator/monitor/check.py
 rm alarm && rm check
